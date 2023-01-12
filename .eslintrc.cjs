82b201e6f4229fbd7e9180a43e653b077155cace
avlcodemonkey <cpittman@gmail.com>
2023-01-12 17:28:34 -0500

    Set up config files for development.

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+    root: true,
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:lit/recommended'
+    ],    
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: ['./tsconfig.json']
+    },
+    plugins: [
+        '@typescript-eslint',
+    ],
+    rules: {
+        'indent': ['error', 4, { 'ignoredNodes': ['TemplateLiteral *'] }],
+        'linebreak-style': [2,'windows'],
+        'class-methods-use-this': 'off',
+        '@typescript-eslint/indent': ['error', 4, { 'ignoredNodes': ['TemplateLiteral *'] }],
+        'max-len': ['error', 200],
+        'import/extensions': 'off',
+        'no-plusplus': 'off',
+        '@typescript-eslint/lines-between-class-members': ['error', 'always', { 'exceptAfterSingleLine': true }],
+        'no-console': ['warn', { 'allow': ['warn', 'error'] }],
+        'no-underscore-dangle': ['error', { 'allow': [ '_index'] }],
+        'no-trailing-spaces': ['error'],
+        'quotes': ['error', 'single'],
+        'comma-dangle': ['error', 'always-multiline'],
+    },
+    ignorePatterns: ['/**/__tests__','.eslintrc.cjs']
+};
