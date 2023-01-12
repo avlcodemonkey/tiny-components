65681195996f57809af1fe7deb999895428cfca3
avlcodemonkey <cpittman@gmail.com>
2023-01-12 17:32:09 -0500

    Use new classes for font icons.

--- src/LitAlert.ts
+++ src/LitAlert.ts
@@ -19,9 +19,10 @@ export class LitAlert extends LitElement {
         if (this.noDismiss) {
             return html`<span class="col-12">${this.msg}</span>`;
         }
+
         return html`
             <span class="col-11">${this.msg}</span>
-            <span class="col-1 pull-right"><i class="lit lit-dismiss btn-cursor" @click="${() => this.isDismissed = true }"></i></span>
+            <span class="col-1 pull-right"><i class="lcc lcc-dismiss btn-cursor" @click="${() => this.isDismissed = true }"></i></span>
         `;
     }
 
