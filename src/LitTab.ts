acd29c6dc84ff161d61d63327894666bcb244149
avlcodemonkey <cpittman@gmail.com>
2023-01-19 17:29:08 -0500

    Simplify tab component code.

--- src/LitTab.ts
+++ src/LitTab.ts
@@ -1,17 +1,10 @@
-import { html, LitElement, unsafeCSS } from 'lit';
+import { html, LitElement } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
-import styles from '../src/styles/index.scss?inline';
 
 @customElement('lit-tab')
 export class LitTab extends LitElement {
     @property({ attribute: 'key' }) key = '';
-
-    @property({ type: Boolean, attribute: 'is-active' }) isActive = false;
-
-    // @todo do i really need styles here?
-    static styles = [
-        unsafeCSS(styles),
-    ];
+    @property({ attribute: 'is-active' }) isActive = false;
 
     render() {
         return html`<slot></slot>`;
