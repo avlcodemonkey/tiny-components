f944a037b7a01e3863a96869f8a159f0441f0b1f
avlcodemonkey <cpittman@gmail.com>
2023-01-20 17:28:56 -0500

    Restyle alert, add i18n property, and wrap icon in button.

--- src/LitAlert.ts
+++ src/LitAlert.ts
@@ -1,29 +1,40 @@
-import { html, LitElement, unsafeCSS } from 'lit';
+import { html, LitElement, unsafeCSS, css } from 'lit';
 import { customElement, property, state } from 'lit/decorators.js';
 import { AlertType } from './enums/AlertType';
 import styles from '../src/styles/index.scss?inline';
 
 @customElement('lit-alert')
 export class LitAlert extends LitElement {
-    @property({ converter: (value) => value ? AlertType[value as keyof typeof AlertType] : undefined }) type: AlertType = AlertType.success
+    @property({ converter: (value) => value ? AlertType[value as keyof typeof AlertType] : undefined }) type: AlertType = AlertType.success;
     @property({ type: Boolean, attribute: 'no-dismiss' }) noDismiss = false;
+    @property({ attribute: 'dismiss-msg' }) dismissMsg = 'Dismiss';
 
     @state() isDismissed = false;
 
     static styles = [
         unsafeCSS(styles),
+        css`
+            col, [class*=" col-"], [class^="col-"] { margin: 0 calc(var(--grid-gutter) / 2) 0 calc(var(--grid-gutter) / 2); }
+            .button-dismiss { border: none; color: inherit; background: inherit; margin-top: -1rem; }
+        `,
     ];
 
     renderInnerContent() {
         if (this.noDismiss) {
-            return html`<span class="col-12"><slot></slot></span>`;
+            return html`<slot></slot>`;
         }
 
         return html`
-            <span class="col-11"><slot></slot></span>
-            <span class="col-1 pull-right">
-                <i class="lcc lcc-dismiss cursor-pointer" @click="${() => this.isDismissed = true}" title="Dismiss" aria-label="Dismiss" role="button"></i>
-            </span>
+            <div class="row">
+                <span class="col-11"><slot></slot></span>
+                <span class="col-1 text-right">
+                    <button class="button icon-only button-dismiss" @click="${() => this.isDismissed = true}" title="${this.dismissMsg}"
+                        aria-label="${this.dismissMsg}"
+                    >
+                        <i class="lcc lcc-dismiss"></i>
+                    </button>
+                </span>
+            </div>
         `;
     }
 
